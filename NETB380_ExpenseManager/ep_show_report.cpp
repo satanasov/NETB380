e2674379659defa8fc614f173e75e06495d287be
bumbuclal <73930558+bumbuclal@users.noreply.github.com>
2021-01-16 03:38:27 +0200

    Changes in ep_report class. Added layout to scroll area widget.

--- NETB380_ExpenseManager/ep_show_report.cpp
+++ NETB380_ExpenseManager/ep_show_report.cpp
@@ -1,5 +1,7 @@
 #include <QDateTime>
-#include <QSpacerItem>
+#include <QVBoxLayout>
+#include <QHeaderView>
+#include <QHBoxLayout>
 #include "ep_show_report.h"
 #include "ui_ep_show_report.h"
 
@@ -8,6 +10,9 @@ ep_show_report::ep_show_report(QWidget *parent) :
     ui(new Ui::ep_show_report)
 {
     ui->setupUi(this);
+    /*/*Add VBoxLayout to the scrollArea.*/
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    ui->scrollAreaReport->widget()->setLayout(mainLayout);
 }
 
 ep_show_report::~ep_show_report()
@@ -26,50 +31,69 @@ void ep_show_report::EP_ShowReport_ProcessReport(QList<QList<QString>> reportDat
     QDateTime dateANDtime = QDateTime::currentDateTime();
     QString dateTime = dateANDtime.currentDateTime().toString();
     ui->label_3->setText(dateTime);
-    /*Add expenses to */
-    for(int i = 0; i < reportData.size(); i++)
+    /*Indexes in reportData.*/
+    int array[5] = {4,6,3,255,7}; // 255 should be subsitute with the currency type of the expense.
+    /*Process all expenses/incomes only if they are available. */
+    if(false == reportData.isEmpty())
     {
-        /*Name of expense.*/
-        QLabel *label = new QLabel(this);
-        label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-        label->setText(reportData.at(i).at(4));
-        label->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
-        /*Type of expense.*/
-        QLabel *label1 = new QLabel(this);
-        label1->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-        label1->setText(reportData.at(i).at(6));
-        label1->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
-        /*Price of expense*/
-        QLabel *label2 = new QLabel(this);
-        label2->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-        label2->setText(reportData.at(i).at(3));
-        label2->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
-        /*Currency of expense.*/
-        QLabel *label3 = new QLabel(this);
-        label3->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-        label3->setText("BGN");
-        label3->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
-        /*Date of expense.*/
-        QDateTime timestamp;
-        timestamp.setTime_t(reportData.at(i).at(7).toUInt());
-        QString expense_date = timestamp.toString();
-        QLabel *label4 = new QLabel(this);
-        label4->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-        label4->setText(expense_date);
-        label4->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
+        for(int i = 0; i < reportData.size(); i++)
+        {
+            /*Create widget container.*/
+            QWidget *newWidgetContainer = new QWidget;
+            /*Create horizontal box layout.*/
+            QHBoxLayout *labelLayout = new QHBoxLayout;
+            /*Create Table widget item and add to table widget.*/
+            for(int j = 0; j < 5; j++)
+            {
+                /*Name of expense.*/
+                QLabel *label = new QLabel();
+                label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+                if(array[j] == 255)
+                {
+                    /*TO DO: Here should be the currency type.*/
+                    label->setText("BGN");
+                }
+                else if(array[j] == 7)
+                {
+                    /*Date of expense-income.*/
+                    QDateTime timestamp;
+                    /*Get date of expense.*/
+                    timestamp.setTime_t(reportData.at(i).at(7).toUInt());
+                    QString expense_date = timestamp.date().toString();
+                    label->setText(expense_date);
+                }
+                else
+                {
+                    /*Take all other data.*/
+                    label->setText(reportData.at(i).at(array[j]));
+                }
+                /*Make center alignment*/
+                label->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
+                /*Add label to layout*/
+                labelLayout->addWidget(label);
+            }
+            /*Set to the container widget all gathered Qlabels.*/
+            newWidgetContainer->setLayout(labelLayout);
+            /*Add to the scrollAreaReport.*/
+            ui->scrollAreaReport->widget()->layout()->addWidget(newWidgetContainer);
 
-        /*Configure gridLayout*/
-//        ui->gridLayout->setColumnMinimumWidth(4,4);
-//        QSpacerItem *spacerItem = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-//        ui->gridLayout->addItem(spacerItem, 1, 1, 1, 1);
-//        QSpacerItem *spacerItem1 = new QSpacerItem(40, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
-//        ui->gridLayout->addItem(spacerItem1, 1, 0, 1, 1);
-        ui->gridLayout->setSpacing(10);
-        /*Add all widgets to grid layout TO DO FIX THIS SHIT :D.*/
-        ui->gridLayout->addWidget(label,i,0,1,1);
-        ui->gridLayout->addWidget(label1,i,1,1,1);
-        ui->gridLayout->addWidget(label2,i,2,1,1);
-        ui->gridLayout->addWidget(label3,i,3,1,1);
-        ui->gridLayout->addWidget(label4,i,4,1,1);
+        }
+    }
+    else
+    {
+        /*Create widget container.*/
+        QWidget *newWidgetContainer = new QWidget;
+        /*Create horizontal box layout.*/
+        QHBoxLayout *labelLayout = new QHBoxLayout;
+        /**/
+        QLabel *label = new QLabel();
+        label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+        label->setText("No data to present. Please add the requested report data firstly.");
+        label->setAlignment(Qt::AlignCenter | Qt::AlignCenter);
+        labelLayout->addWidget(label);
+        /*Set to the container widget all gathered Qlabels.*/
+        newWidgetContainer->setLayout(labelLayout);
+        /*Add to the scrollAreaReport.*/
+        ui->scrollAreaReport->widget()->layout()->addWidget(newWidgetContainer);
     }
 }
